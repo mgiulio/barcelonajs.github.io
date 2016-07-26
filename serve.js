814e5e0c3b4a21648c10fb60926cd479354df86f
mgiulio <giulio.mainardi@gmail.com>
2016-07-26 12:09:16 +0200

    serve.js: fix indentation

--- serve.js
+++ serve.js
@@ -163,14 +163,14 @@ function bumpTalkUrl() {
 }
 
 function truncate(str, len) {
-    if (str.length > len && str.length > 0) {
-        var new_str = str + " ";
-        new_str = str.substr(0, len);
-        new_str = str.substr(0, new_str.lastIndexOf(" "));
-        new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
-        return new_str + '...' ; 
-    }
-    return str;
+  if (str.length > len && str.length > 0) {
+    var new_str = str + " ";
+    new_str = str.substr(0, len);
+    new_str = str.substr(0, new_str.lastIndexOf(" "));
+    new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+    return new_str + '...' ; 
+  }
+  return str;
 };
 
 Metalsmith(__dirname)
